a5eac906a2abde129d61620e18a796d7b1d9ee1f
Jeyther <jeyther13@gmail.com>
2022-02-08 23:46:27 -0500

    adding a NavBar!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <div>
+
+          <NavBar/>
+
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
